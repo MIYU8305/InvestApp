33e4c5edf32912088a3c7cbb44552de4009b80f8
MIYU8305 <63732178+MIYU8305@users.noreply.github.com>
2026-04-19 13:20:41 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
